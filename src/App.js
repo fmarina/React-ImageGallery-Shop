a6488dc8a1c10536d0d28df73659ae68a95d140f
Marina <marinafaenze@hotmail.com>
2020-07-13 16:31:18 -0300

    React Router set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css'
 import Header from './components/Header';
+import Photos from './pages/Photos';
+import Cart from './pages/Cart';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
       <Header />
+
+      <Switch>
+        <Route exact path="/"><Photos/></Route>
+        <Route path="/cart"><Cart/></Route>
+      </Switch>
+      
       <h1>Home page</h1>
     </div>
   );
