e2619517b55a7e1df5050defdbdd329d8189c175
Marina <marinafaenze@hotmail.com>
2020-07-13 15:57:34 -0300

    App component created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './App.css'
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
+    <div className="app">
+      <Header />
+      <h1>Home page</h1>
     </div>
   );
 }
