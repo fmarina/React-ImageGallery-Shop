80968c9aa98470b835370cf3e5b3376ea7156f6b
Marina <marinafaenze@hotmail.com>
2020-07-14 20:05:18 -0300

    minor ux improvements at card page

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,13 +1,18 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {Context} from '../Context';
 
 const CartItem = ({item}) => {
+    const [hovered, setHovered] = useState(false);
     const {removeImageFromCart} = useContext(Context);
+
+    const iClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line";
 
     return (
         <div className="cart-item">           
-            <i className="ri-delete-bin-line" 
-               onClick={() => removeImageFromCart(item.id)}>
+            <i className={iClassName} 
+               onClick={() => removeImageFromCart(item.id)}
+               onMouseEnter={() => setHovered(true)}
+               onMouseLeave={() => setHovered(false)}>
             </i>
             <img src={item.url} alt="a pic" width="130px" />
             <p>$5.99</p>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -28,9 +28,13 @@ const Cart = () => {
             <div className="container-cart">
                 {cartItemElements}
                 <p className="total-cost">Total: {totalCostDisplay}</p>
-                <div className="order-button">
-                    <button onClick={placeOrder}>{buttonText}</button>
-                </div>
+                {
+                    cartItems.length > 0 
+                    ? <div className="order-button">
+                        <button onClick={placeOrder}>{buttonText}</button>
+                      </div> 
+                    : <p>You have no items in your cart.</p>
+                }
             </div>
         </main>
     );
